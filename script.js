08deed99d286e2d74e7ab5f76023bcb563b6164f
DakMorg <108765820+DakMorg@users.noreply.github.com>
2022-07-30 18:43:40 -0400

    update to starter code

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var testName = "frank";
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-console.log(testName);
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
